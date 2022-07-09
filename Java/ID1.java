989974806bf2081a9498133adff5bbf566ded87a
IceW0lf <michaelsinnecker@web.de>
2022-07-09 14:31:18 +0200

    ID1 solution - java

---
+++ Java/ID1.java
@@ -0,0 +1,28 @@
+/*
+https://projecteuler.net/problem=1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+package Java;
+
+public class ID1 {
+
+    public static void main(String[] args) {
+        System.out.println(calcSum(10));
+        System.out.println(calcSum(1000));
+    }
+
+    public static Integer calcSum(int limit) {
+        Integer sum = 0;
+
+        for (int i = 1; i < limit; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
