4d18dbaf0be55771322b73e0b82dbb0dbe30a2b2
IceW0lf <michaelsinnecker@web.de>
2022-07-10 13:09:36 +0200

    utilizing MethodCollection

--- Java/ID7.java
+++ Java/ID7.java
@@ -13,31 +13,11 @@ public class ID7 {
 
     private static Integer calcPrimeByPosition(Integer pos) {
         for (int counter = 0, primeCandidate = 1;; primeCandidate++) {
-            if (primeCheck(primeCandidate)) {
+            if (MethodCollection.primeCheck(primeCandidate)) {
                 if (++counter == pos) {
                     return primeCandidate;
                 }
             }
         }
-    }
-
-    private static Boolean primeCheck(Integer primeCandidate) {
-        Integer flooredSqrtOfPrimeCandidate = (int) Math.floor(Math.sqrt(primeCandidate));
-
-        if (primeCandidate == 2) {
-            return true;
-        }
-
-        if (primeCandidate < 2 || primeCandidate % 2 == 0) {
-            return false;
-        }
-
-        for (int i = 3; i <= flooredSqrtOfPrimeCandidate; i += 2) {
-            if (primeCandidate % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
