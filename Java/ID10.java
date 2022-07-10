3c29c33f5d62785a5e909bffd6e0f14e905f0b2b
IceW0lf <michaelsinnecker@web.de>
2022-07-10 13:09:50 +0200

    ID10 solution - java

---
+++ Java/ID10.java
@@ -0,0 +1,25 @@
+/*
+ * https://projecteuler.net/problem=10
+ */
+
+package Java;
+
+public class ID10 {
+    
+    public static void main(String[] args) {
+        System.out.println(calcSumOfPrimesBelow(10));
+        System.out.println(calcSumOfPrimesBelow(2000000));
+    }
+
+    public static Long calcSumOfPrimesBelow(Integer limit) {
+        Long sum = 0L;
+        
+        for (int i = 0; i < limit; i++) {
+            if(MethodCollection.primeCheck(i)) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }     
+}
