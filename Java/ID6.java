e983dbd238bb28538a4a3aae494198cf4ab40149
IceW0lf <michaelsinnecker@web.de>
2022-07-10 07:32:14 +0200

    ID6 solution - java

---
+++ Java/ID6.java
@@ -0,0 +1,35 @@
+/* 
+ * https://projecteuler.net/problem=6
+ * 
+ * The sum of the squares of the first ten natural numbers is,
+ *     1^2 + 2^2 + ... + 10^2 = 385
+ * 
+ * The square of the sum of the first ten natural numbers is,
+ *      (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ * 
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+ * 
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+
+package Java;
+
+public class ID6 {
+
+    public static void main(String[] args) {
+        System.out.println(calc(10));
+        System.out.println(calc(100));
+    }
+
+    public static Integer calc(Integer limit) {
+        Integer sum = 0;
+        Integer sumOfSquares = 0;
+
+        for (int i = 1; i <= limit; i++) {
+            sumOfSquares += i * i;
+            sum += i;
+        }
+
+        return sum * sum - sumOfSquares;
+    }
+}
