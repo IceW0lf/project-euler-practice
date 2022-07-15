c0ad5b1ce3fe12387dd8bcc598b148eb7fbc1d50
IceW0lf <michaelsinnecker@web.de>
2022-07-15 10:29:45 +0200

    minor refactoring

--- Java/ID017.java
+++ Java/ID017.java
@@ -7,11 +7,11 @@ package Java;
 public class ID017 {
 
     public static void main(String[] args) {
-        System.out.println(calcNumberOfLetters(5));
-        System.out.println(calcNumberOfLetters(1000));
+        System.out.println(calcLetterCountOfNumbers(5));
+        System.out.println(calcLetterCountOfNumbers(1000));
     }
 
-    public static Integer calcNumberOfLetters(Integer max) {
+    public static Integer calcLetterCountOfNumbers(Integer max) {
         Integer sum = 0;
 
         for (int i = 1; i <= max; i++) {
@@ -25,13 +25,13 @@ public class ID017 {
         if (number < 20) {
             return zeroToNineteen[number];
         } else if (number >= 20 && number < 100) {
-            return twentyToNinety[number / 10 - 2] + (number % 10 != 0 ? zeroToNineteen[number % 10] : "");
+            return twentyToNinety[number / 10 - 2] + (number % 10 != 0 ? numberToString(number % 10) : "");
         } else if (number >= 100 && number < 1000) {
             return zeroToNineteen[number / 100] + "hundred" + (number % 100 != 0 ? "and" + numberToString(number % 100) : "");
         } else if (number == 1000) {
             return "onethousand";
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("enter number equal or less than 1000");
         }
     }
 
